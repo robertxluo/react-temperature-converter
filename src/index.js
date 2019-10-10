47eebdac4ab20beb0660cdef1abb749bdfe171da
robertxluo <robertxluo@gmail.com>
2019-10-09 19:37:33 -0500

    Refactor App component to index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
