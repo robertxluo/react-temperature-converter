a32685b3c8d98c04edcf922c3c211004c41cef9e
robertxluo <robertxluo@gmail.com>
2019-10-09 19:45:51 -0500

    Add Button component

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+import Button from './components/Button';
+
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Button>Normal Button</Button>
+      <Button primary>Primary Button</Button>
     </div>
   );
 }
